bf844e69982afdc49ae37be99cffdd2d38251451
micihime <hroncovam@yahoo.com>
2024-02-05 11:25:40 +0100

    Using INotifyPropertyChanged attribute from CommunityToolkit.Mvvm - source code for the properties and notifying about it being changed are generated automatically

--- MonkeyFinder/ViewModel/BaseViewModel.cs
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,46 +1,11 @@
 ﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel : INotifyPropertyChanged
+[INotifyPropertyChanged]
+public partial class BaseViewModel 
 {
     bool isBusy;
     string title;
-
-    public bool IsBusy
-    {
-        get => isBusy;
-        set
-        {
-            if (isBusy == value)
-                return;
-
-            isBusy = value;
-            OnPropertyChanged(); //notify that this has changed
-            OnPropertyChanged(nameof(IsNotBusy)); 
-        }
-    }
-
-    public bool IsNotBusy => !IsBusy;
-
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title == value)
-                return;
-
-            title = value;
-            OnPropertyChanged(); //notify that this has changed
-        }
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    public void OnPropertyChanged([CallerMemberName]string name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
 }
