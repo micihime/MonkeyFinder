cc17b640f837e21daaa32fbf4135dbb0b369ad80
micihime <hroncovam@yahoo.com>
2024-02-05 10:15:22 +0100

    Adding common properties to BaseViewModel

--- MonkeyFinder/ViewModel/BaseViewModel.cs
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,28 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using System.ComponentModel;
 
-public class BaseViewModel
+namespace MonkeyFinder.ViewModel;
+
+public class BaseViewModel : INotifyPropertyChanged
 {
+    bool isBusy;
+
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+
+            isBusy = value;
+            OnPropertyChanged(nameof(IsBusy)); //notify that this has changed
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
