830ffadd01e4d92dfefed16264822250374dc7c2
micihime <hroncovam@yahoo.com>
2024-02-05 11:59:14 +0100

    Adding dummy service method GetMonkeys

--- MonkeyFinder/Services/MonkeyService.cs
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,11 @@
-﻿namespace MonkeyFinder.Services;
+﻿using MonkeyFinder.Model;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        return null; // new List<Monkey>();
+    }
 }
