ceef1af459b6bcd3ab9e9c80515ec4f7706e4e5f
micihime <hroncovam@yahoo.com>
2024-02-06 10:59:22 +0100

    Creating monkey list in the VM, setting the title of the page

--- MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,18 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Model;
+using MonkeyFinder.Services;
+using System.Collections.ObjectModel;
+
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;
+
+    public ObservableCollection<Monkey> Monkeys { get; } = new ();
+
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
 }
